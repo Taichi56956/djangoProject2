2f292fcbe3b174cfd435af0a2dc83d341d4f0b7b
MutistTaichi <th856775@gmail.com>
2023-02-02 11:51:29 +0900

    1

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from app import views
+from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('fib/<str:n>/', views.fibonacci_view, name='fibonacci'),
 ]
